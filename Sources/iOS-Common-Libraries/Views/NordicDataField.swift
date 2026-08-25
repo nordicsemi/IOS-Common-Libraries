4d88cef6ce3c3f4013fb1176b122a6bfb6dd5f44
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2026-08-25 09:29:35 +0100

    Fixes to NordicDataField
    - Customizable background color, there was no alternative left
    - By adding the outer VStack, .focusedField() now works when using externally
    - Small fix for vertical padding

--- Sources/iOS-Common-Libraries/Views/NordicDataField.swift
+++ Sources/iOS-Common-Libraries/Views/NordicDataField.swift
@@ -15,65 +15,75 @@ import SwiftUI
 @available(iOS 15.0, macCatalyst 15.0, macOS 12.0, *)
 public struct NordicDataField: View {
     
+    #if os(iOS) || targetEnvironment(macCatalyst)
+    public static let DefaultBackgroundColor: Color = Color(.systemGray6)
+    #else
+    public static let DefaultBackgroundColor: Color.secondarySystemBackground
+    #endif
+    
     // MARK: Properties
     
     @Binding private var data: Data
     @Binding private var selectedParser: CommonDataParser
+    
     @State private var dataBool: Bool
     @State private var dataString: String
+    
     private let dataParsers: [CommonDataParser]
+    private let backgroundColor: Color
     private let dataParserPickerEnabled: Bool
     
-    // MARK: Init
+    // MARK: init
     
     public init(data: Binding<Data>, dataParser: Binding<CommonDataParser>,
-         dataParsers: [CommonDataParser], parserIsUserSelectable: Bool = true) {
+                dataParsers: [CommonDataParser], backgroundColor: Color = Self.DefaultBackgroundColor,
+                parserIsUserSelectable: Bool = true) {
         self._data = data
         self._selectedParser = dataParser
         self.dataBool = false
         self.dataString = dataParser.wrappedValue(data.wrappedValue) ?? ""
         self.dataParsers = dataParsers
+        self.backgroundColor = backgroundColor
         self.dataParserPickerEnabled = parserIsUserSelectable
     }
     
-    // MARK: View
+    // MARK: view
     
     public var body: some View {
-        if selectedParser == .boolean {
-            Toggle(dataBool ? "True (0x1)" : "False (0x0)", isOn: $dataBool)
-                .onChange(of: dataBool) { newValue in
-                    data = Data(repeating: newValue ? 1 : 0, count: 1)
-                }
-        } else {
-            TextField("", text: $dataString)
-                .padding(4)
-                #if os(iOS) || targetEnvironment(macCatalyst)
-                .background(Color(.systemGray6))
-                .textInputAutocapitalization(.never)
-                #else
-                .background(Color.secondarySystemBackground)
-                #endif
-                .cornerRadius(8)
-                .autocorrectionDisabled()
-                .onChange(of: dataString) { newString in
-                    switch selectedParser {
-                    case .byteArray:
-                        data = Data(hexString: newString) ?? Data()
-                    case .unsignedInt:
-                        guard let number = UInt8(newString) else { return }
-                        data = Data(repeating: number, count: 1)
-                    case .boolean:
-                        // No-op, because the Toggle should be visible.
-                        data = Data()
-                    case .utf8:
-                        data = newString.data(using: .utf8) ?? Data()
-                    default:
-                        return
+        VStack {
+            if selectedParser == .boolean {
+                Toggle(dataBool ? "True (0x1)" : "False (0x0)", isOn: $dataBool)
+                    .onChange(of: dataBool) { newValue in
+                        data = Data(repeating: newValue ? 1 : 0, count: 1)
                     }
-                }
+            } else {
+                TextField("", text: $dataString)
+                    .padding(4)
+                    .background(backgroundColor)
+                    .cornerRadius(8)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .onChange(of: dataString) { newString in
+                        switch selectedParser {
+                        case .byteArray:
+                            data = Data(hexString: newString) ?? Data()
+                        case .unsignedInt:
+                            guard let number = UInt8(newString) else { return }
+                            data = Data(repeating: number, count: 1)
+                        case .boolean:
+                            // No-op, because the Toggle should be visible.
+                            data = Data()
+                        case .utf8:
+                            data = newString.data(using: .utf8) ?? Data()
+                        default:
+                            return
+                        }
+                    }
+            }
+            
+            InlineSegmentedControlPicker(selectedValue: $selectedParser, possibleValues: dataParsers)
+                .disabled(!dataParserPickerEnabled)
         }
-        
-        InlineSegmentedControlPicker(selectedValue: $selectedParser, possibleValues: dataParsers)
-            .disabled(!dataParserPickerEnabled)
+        .padding(.vertical, 2.0)
     }
 }
